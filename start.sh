794762844e0247432792544df65494257ea04c46
Administrator <admin@example.com>
2021-01-22 20:53:11 +0400

    rename

--- start.sh
+++ start.sh
@@ -1,3 +1,1 @@
-cd /home/user/PycharmProjects/time_tracker
-export DISPLAY=:0
-venv/bin/python main.py
+cd /home/user/PycharmProjects/time_tracker && export DISPLAY=:0 && venv/bin/python time_tracker.py
